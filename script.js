d08a19d60b0478e1bdf7198e6b5fb051fb13cb19
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-03-28 14:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let x = prompt("Enter First Number: ");
+let res1 = x*2;
+let y = prompt("Enter Second Number: ");
+let res2 = Number(y) + Number(res1);
+alert("The Final result is: "+ res2);
